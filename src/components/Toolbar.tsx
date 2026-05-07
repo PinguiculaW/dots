3fac8e91166a0e634415f45b09b0d0988e451aa6
PinguiculaW <maripul.ka0066@gmail.com>
2026-05-07 20:40:05 +0300

    use Mui (#4)
    Co-authored-by: TimPulin <tigeronsun@gmail.com>

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 import BrailleEditorModal from "./BrailleEditorModal";
-import { useState } from "react";
-import type { Tool, Selection } from "../types";
+import {useState} from "react";
+import type {Tool, Selection} from "../types";
+import Button from '@mui/material/Button';
+
 
 interface ToolbarProps {
   selectedTool: Tool;
@@ -21,104 +23,107 @@ interface ToolbarProps {
 }
 
 const Toolbar: React.FC<ToolbarProps> = ({
-  selectedTool,
-  setSelectedTool,
-  brushSize,
-  setBrushSize,
-  selectedSymbol,
-  setSelectedSymbol,
-  undo,
-  redo,
-  resizeGrid,
-  copySelection,
-  width,
-  height,
-  setSelection,
-}) => {
-    const [showModal, setShowModal] = useState(false);
-    const toolButton = (tool: Tool, icon: string) => (
-        <button
-            onClick={() => {
-                setSelectedTool(tool);
-                setSelection(null); // 🔥 вот это ключ
-            }}
-            className={selectedTool === tool ? "tool active" : "tool"}
-        >
-            {icon}
-        </button>
-    );
+                                           selectedTool,
+                                           setSelectedTool,
+                                           brushSize,
+                                           setBrushSize,
+                                           selectedSymbol,
+                                           setSelectedSymbol,
+                                           undo,
+                                           redo,
+                                           resizeGrid,
+                                           copySelection,
+                                           width,
+                                           height,
+                                           setSelection,
+                                         }) => {
+  const [showModal, setShowModal] = useState(false);
+  const toolButton = (tool: Tool, icon: string) => (
+    <Button
+      variant='outlined'
+      onClick={() => {
+        setSelectedTool(tool);
+        setSelection(null); // 🔥 вот это ключ
+      }}
+      className={selectedTool === tool ? "tool active" : "tool"}
+    >
+      {icon}
+    </Button>
+  );
 
   return (
     <div className="toolbar">
-        <div className="group">
-      {toolButton("pencil", "✏️")}
-            {toolButton("eraser", "🧽")}
-      {toolButton("picker", "🎯")}
-      {toolButton("fill", "🪣")}
-        </div>
+      <div className="group">
+        {toolButton("pencil", "✏️")}
+        {toolButton("eraser", "🧽")}
+        {toolButton("picker", "🎯")}
+        {toolButton("fill", "🪣")}
+      </div>
 
-        <div className="group">
-      {toolButton("select", "▢")}
+      <div className="group">
+        {toolButton("select", "▢")}
         <button onClick={copySelection}>📋</button>
-            {toolButton("paste", "📥")}
-        </div>
+        {toolButton("paste", "📥")}
+      </div>
 
-        <div className="group">
-            <button onClick={undo}>↩</button>
-            <button onClick={redo}>↪</button>
-        </div>
+      <div className="group">
+        <Button sx={{p:0}} variant='outlined'  onClick={undo}>↩</Button>
+        <Button sx={{p:0}} variant='outlined'  onClick={redo}>↪</Button>
+      </div>
 
-        {showModal && (
-            <BrailleEditorModal
-                initial={selectedSymbol}
-                onApply={(value) => {
-                    setSelectedSymbol(value);
-                    setShowModal(false);
-                }}
-                onClose={() => setShowModal(false)}
-            />
-        )}
-
-      <label>
-        Размер:
-        <input
-          type="number"
-          min={1}
-          max={5}
-          value={brushSize}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setBrushSize(+e.target.value)
-          }
+      {showModal && (
+        <BrailleEditorModal
+          initial={selectedSymbol}
+          onApply={(value) => {
+            setSelectedSymbol(value);
+            setShowModal(false);
+          }}
+          onClose={() => setShowModal(false)}
         />
-      </label>
+      )}
+      <div className="group">
+        <label>
+          Размер:
+          <input
+            type="number"
+            min={1}
+            max={5}
+            value={brushSize}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setBrushSize(+e.target.value)
+            }
+          />
+        </label>
+      </div>
 
-      <button onClick={() => resizeGrid(20, 10)}>20×10</button>
-      <button onClick={() => resizeGrid(40, 20)}>40×20</button>
-
+      <div className="group">
+        <Button sx={{p:0}} variant='outlined' onClick={() => resizeGrid(20, 10)}>20×10</Button>
+        <Button sx={{p:0}} variant='outlined' onClick={() => resizeGrid(40, 20)}>40×20</Button>
+      </div>
       {/* Используем setSelectedSymbol */}
       <input
         type="text"
         value={selectedSymbol}
         onChange={(e) => setSelectedSymbol(e.target.value)}
         maxLength={1}
-        style={{ width: 30, marginLeft: 10 }}
+        style={{width: 30, marginLeft: 10}}
       />
-        <button onClick={() => setShowModal(true)}>⚙️</button>
-        <div className="grid-size-controls">
-            <div className="control">
-                <label>Столбцы</label>
-                <button onClick={() => resizeGrid(width + 1, height)}>+</button>
-                <input type="number" value={width} readOnly />
-                <button onClick={() => resizeGrid(width - 1, height)}>-</button>
-            </div>
-
-            <div className="control">
-                <label>Строки</label>
-                <button onClick={() => resizeGrid(width, height + 1)}>+</button>
-                <input type="number" value={height} readOnly />
-                <button onClick={() => resizeGrid(width, height - 1)}>-</button>
-            </div>
+      <button onClick={() => setShowModal(true)}>⚙️</button>
+      <div className="grid-size-controls">
+        <div className="control">
+          <label>Столбцы</label>
+          <button onClick={() => resizeGrid(width + 1, height)}>+</button>
+          <input type="number" value={width} readOnly/>
+          <button onClick={() => resizeGrid(width - 1, height)}>-</button>
         </div>
+
+        <div className="control">
+          <label>Строки</label>
+          <button onClick={() => resizeGrid(width, height + 1)}>+</button>
+          <input type="number" value={height} readOnly/>
+          <button onClick={() => resizeGrid(width, height - 1)}>-</button>
+        </div>
+      </div>
     </div>
   );
 };
