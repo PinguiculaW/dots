c5979d615e34f2f59395eb91f431aa096b22846c
Pinguicula <maripul.ka0066@gmail.com>
2026-05-12 20:49:42 +0300

    письма с сайта отправляются на почту (gmail)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import AppInner from './AppInner';
+import FeedbackForm from "./components/FeedbackForm";
 
 export default function App() {
   return (
     <>
       <h1></h1>
       <AppInner />
+        <FeedbackForm />
     </>
   );
 }
